8908a73d1980505487328599470226971f91884b
AL AJROUDI <alexandre7@hotmail.fr>
2020-05-24 18:12:14 +0200

    OBJECTIF 1TERMINE

---
+++ Src/principal.c
@@ -0,0 +1,16 @@
+
+extern int modulo(int,unsigned short *);
+extern unsigned short TabSig[64];
+int tab[64];
+int main(void)
+{
+
+for(int k = 0 ; k<64;k++){
+ tab[k] = modulo(k,TabSig);		
+}
+
+
+while	(1)
+	{
+	}
+}
